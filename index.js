866052a9909276ab44f9888accf9a6fb9e0f79e9
abrar-11 <abrarahmed2185@gmail.com>
2023-02-21 19:15:50 +0500

    bug

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
-import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js'
-import dalleRoutes from './routes/dalleRoutes.js'
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
-
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
-app.use('/api/v1/post', postRoutes)
-app.use('/api/v1/dalle', dalleRoutes)
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
-app.get('/', async (req, res) => {
-  res.send('Hello from DALL-E');
+app.get("/", async (req, res) => {
+    res.send("Hello from DALL-E");
 });
 
 const startServer = () => {
-
     try {
-        // connectDB(process.env.MONGODB_URL)
-        app.listen(5000, () =>
-        console.log('Server is listening on port http://localhost:5000')
-      );
+        connectDB(process.env.MONGODB_URL);
+        app.listen(port, () =>
+            console.log("Server is listening on port http://localhost:", port)
+        );
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-
 };
 
-
-startServer()
+startServer();
